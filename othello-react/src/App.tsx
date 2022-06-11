f9629fab1aef17eed70040935ac010aa1efa07af
22ono <ono-hiroto385@g.ecc.u-tokyo.ac.jp>
2022-06-11 14:42:43 +0900

    daimei

--- othello-react/src/App.tsx
+++ othello-react/src/App.tsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+    <>
+      <div className="title"><h1>無料オンラインオセロゲーム</h1></div>
+      <div className="main">
+        <p >これは無料でオンライン上の世界の人々と自由にオセロの対戦を楽しむことのできるサイトです。</p>
+        <p>遊び方
+          <ul>
+            <li>自分の手番になったらマス目をクリックして、相手の駒を挟みましょう</li>
+            <li>もし、駒をおける場所がなくなったら、"パス"ボタンを押して相手に手番を譲りましょう</li>
+          </ul>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+      </div>
 
-export default App
+      <table>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </>
+  );
+}
